9f5dec50e9b35cf23a581c92236191ec5ebc7c45
Jennifer Fong <jen@ednapiranha.com>
2014-06-03 17:03:02 -0700

    i hope this ascii idea wont be regretful

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var irc = require('irc');
 var nconf = require('nconf');
 var Twitter = require('./lib/twitter');
+var Ascii = require('./lib/ascii');
 
 nconf.argv().env().file({ file: 'config.json' });
 
 var tw = new Twitter(nconf);
+var ascii = new Ascii();
 
 var client = new irc.Client(nconf.get('server'), nconf.get('botName'), {
   channels: nconf.get('channels'),
@@ -14,7 +16,7 @@ var client = new irc.Client(nconf.get('server'), nconf.get('botName'), {
 
 nconf.get('channels').forEach(function (channel) {
   client.join(channel, function () {
-    console.log('connected to ', channel)
+    console.log('connected to ', channel);
     client.say('NickServ', 'IDENTIFY ' + nconf.get('password'));
   });
 });
@@ -24,19 +26,20 @@ client.addListener('registered', function (message) {
 });
 
 client.addListener('message', function (from, to, message) {
-  console.log(from + ' => ' + to + ': ' + message);
-
   if (message.match(/^(\.vw )/i)) {
     tw.tweet(message.split('.vw ')[1], function (err, tid) {
       if (err) {
         console.error(err);
       } else {
-        client.say(to, 'tweeted: https://twitter.com/virtualwhatever/status/' + tid);
+        client.say(to, 'tweeted: https://twitter.com/' + nconf.get('twitterNick') + '/status/' + tid);
       }
     });
+  } else if (message.match(/^(\.draw )/i)) {
+    var face = ascii.draw(message.split('.draw ')[1].split(' ')[0]);
+    client.say(to, face);
   }
 });
 
 client.addListener('error', function (message) {
-  console.log('error: ', message);
+  console.error('error: ', message);
 });

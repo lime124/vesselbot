9dbef3c280c9f51422c42c651b0235ff32cda310
Avery Lewis <avery.lewis@gmail.com>
2014-06-05 21:10:25 -0700

    adding doge

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var nconf = require('nconf');
 var Twitter = require('./lib/twitter');
 var Ascii = require('./lib/ascii');
 var Cats = require ('./lib/cats');
+var Dogs = require ('./lib/dogs');
 
 nconf.argv().env().file({ file: 'config.json' });
 
 var tw = new Twitter(nconf);
 var ascii = new Ascii();
 var cats = new Cats();
+var dogs = new Dogs();
 
 var client = new irc.Client(nconf.get('server'), nconf.get('botName'), {
   channels: nconf.get('channels'),
@@ -40,6 +42,8 @@ client.addListener('message', function (from, to, message) {
     client.say(to, ascii.draw(message.split('.draw ')[1]));
   } else if (message.match(/^(\.cat )/i)) {
     client.say(to, cats.pic(message.split('.cat ')[1]));
+  } else if (message.match(/^(\.dog )/i)) {
+    client.say(to, dogs.pic(message.spli('.dog ')[1]));
   }
 });
 
